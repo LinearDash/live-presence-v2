76a2d1e76abe548c0bf72e2b57a0fccf63d07ec2
LinearDash <asandesh495@gmail.com>
2025-11-07 20:34:08 +0545

    add delete user functionality with error handling and validation

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -41,6 +41,28 @@ export const createUser = async (req: Request, res: Response) => {
   }
 }
 export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.body;
+
+    if (!id) {
+      return res.status(400).json({ error: "UserId is required" })
+    }
+
+    const searchUser = await prisma.user.findUnique({
+      where: { id },
+    })
+    if (!searchUser) {
+      return res.status(404).json({ error: "User not found" })
+    }
+
+    await prisma.user.delete({
+      where: { id },
+    })
+    return res.status(200).json({ message: "User Deleted Successfully" })
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 
 }
 export const getAllUser = async (req: Request, res: Response) => {
