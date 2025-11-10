8a9388509df9246e368f15ea8db0bb4198721b33
LinearDash <asandesh495@gmail.com>
2025-11-10 16:38:48 +0545

    add logout route to authentication routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller";
+import { login, logout, register } from "../controllers/auth.controller";
 
 const router = express.Router()
 
 router.post("/register", register);
 
 router.post('/login', login)
+
+router.post('/logout', logout)
 
 export default router;
